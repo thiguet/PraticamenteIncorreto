154cc974cac0e558d37dde31971dda88a60afcee
Thiguet <thiguet2014@gmail.com>
2017-04-14 17:15:09 -0300

    Agora podemos mexer nas parada

---
+++ front/assets/js/angularInit.js
@@ -0,0 +1,5 @@
+var app = angular.module("praticamenteIncorreto", []);
+app.controller('cabecalhoController', function($http) {
+
+});
+
---
+++ front/dist/app.js
@@ -0,0 +1,7 @@
+// FILE: ./assets/js/angularInit.js
+var app = angular.module("praticamenteIncorreto", []);
+app.controller('cabecalhoController', function($http) {
+
+});
+
+
