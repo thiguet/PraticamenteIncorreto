019e4cb65e870fe13e2714cc2be20886a2196fc5
thiguet <thiguet2014@gmail.com>
2017-05-20 14:03:05 -0300

    O ScreenInit mudou.

--- front/assets/js/screenInit.js
+++ front/assets/js/screenInit.js
@@ -1,4 +1,5 @@
 function screenInit ($scope, $http) {
+	var config;
 	$(document).ready(function() {
 		// Add smooth scrolling to all links in navbar + footer link
 		$(" .menu-item a , .navbar a , footer a[href='#HOME']").on('click', function(event) {
@@ -25,61 +26,12 @@ function screenInit ($scope, $http) {
 		});
 	});
 
-	$scope.welcome = "E ai gente? Sejam bem vindos ao nosso site! Nós o fizemos para ser um espaço mais perto entre nós e vocês. Espero que gostem!",
-
-    $scope.menu = [
-        "HOME",
-        "BANDA",
-        "SHOWS",
-        "CONTATO"
-    ];
-
-    $scope.banda = {
-        "nome": "Praticamente Incorreto",
-        "email": "bandapraticamenteincorreto@gmail.com",
-        "integrantes": [
-            {
-                "nome": "Thiguet",
-                "descricao": "Adora falar coisas sem sentido durante o show.",
-                "tel": "+55 (31) 98221-6556",
-                "papeis": "Guitarrista e Vocalista",
-                "anoEntrada": 2014
-            },
-            {
-                "nome": "Barbosa",
-                "descricao": "Ama girar e jogar a baqueta durante o show.",
-                "tel": "+55 (31) 99296-9209",
-                "papeis": "Baterista",
-                "anoEntrada": 2014
-            },
-            {
-                "nome": "Leo",
-                "descricao": "Ama fazer uns solo lokos durante o show.",
-                "tel": "+55 (31) 99246-1009",
-                "papeis": "Guitarrista Principal",
-                "anoEntrada": 2017
-            }
-        ]
-    };
-
-	$scope.shows = [
-		{
-			"titulo": "Barzinho A",
-			"data": "Sexta, 25 de maio de 2017",
-			"img": "pub a.jpg"
-		},
-		{
-			"titulo": "New York",
-			"data": "Sábado, 28 de novembro de 2015",
-			"img": "pub b.jpg"
-		},
-		{
-			"titulo": "São Francisco",
-			"data": "Domingo, 29 de novembro de 2015",
-			"img": "pub a.jpg"
-		}
-	];
+	$http.get( './assets/json/bandaInfo.json').then(function(result) {
+		config = result.data;
+		$scope.menu = config.menu;
+		$scope.welcome = config.welcome;
+		$scope.banda = config.banda;
+		$scope.shows = config.shows;
+	});
 }
-
-
 RegisterInAngular('screenInit', screenInit);
